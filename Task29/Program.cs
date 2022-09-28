7afa4a3c4ce36286c61f3dcb26e1db4a4266813d
Vitaly <vivlgud@mail.ru>
2022-09-28 16:44:16 +0200

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int[] array=new int[8];
+Console.WriteLine("Введите 8 чисел для заполнения массива");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]=Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Полученный массив ");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i<array.Length-1) Console.Write($"{array[i]},");
+    else Console.Write($"{array[i]}");
+}
+Console.Write("]");
